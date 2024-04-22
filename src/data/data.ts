d9d748f5a6272ab2b6caaedf9bf9be3db88e15c7
Clara <clarafdezfraga@gmail.com>
2024-04-22 23:54:13 +0200

    Add joffrey, jaime and daenerys data

---
+++ src/data/data.ts
@@ -0,0 +1,30 @@
+import { type Knight, type King } from "../types.js";
+
+const joffreyBaratheon: King = {
+  name: "Joffrey",
+  familyName: "Baratheon",
+  age: 14,
+  status: "deceased",
+  totalReignYears: 2,
+  locution: "Vais a morir todos",
+};
+
+const jaimeLannister: Knight = {
+  name: "Jaime",
+  familyName: "Lannister",
+  age: 34,
+  status: "alive",
+  weapon: "Lamento de Viuda",
+  dexterity: 9,
+  locution: "Primero pego y luego pregunto",
+};
+
+const daenerysTargaryen: Knight = {
+  name: "Daenerys",
+  familyName: "Targaryen",
+  age: 23,
+  status: "deceased",
+  weapon: "Drogon",
+  dexterity: 10,
+  locution: "Primero pego y luego pregunto",
+};
--- src/types.ts
+++ src/types.ts
@@ -5,23 +5,23 @@ export interface Character {
   status: "alive" | "deceased";
 }
 
-interface King extends Character {
+export interface King extends Character {
   totalReignYears: number;
   locution: "Vais a morir todos";
 }
 
-interface Knight extends Character {
+export interface Knight extends Character {
   weapon: string;
   dexterity: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
   locution: "Primero pego y luego pregunto";
 }
 
-interface Counselor extends Character {
+export interface Counselor extends Character {
   counselledCharacter: Character;
   locution: "No sé por qué, pero creo que voy a morir pronto";
 }
 
-interface Squire extends Character {
+export interface Squire extends Character {
   master: Knight;
   GrovellingLevel: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
   locution: "Soy un loser";
