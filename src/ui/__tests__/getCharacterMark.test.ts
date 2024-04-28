3660c7ac6c0defa54765a2f01acc22c1566c09f4
Clara <clarafdezfraga@gmail.com>
2024-04-28 17:50:48 +0200

    Add getCharacterKindMark

---
+++ src/ui/__tests__/getCharacterMark.test.ts
@@ -0,0 +1,129 @@
+import exp from "constants";
+import {
+  createCounselor,
+  createFighter,
+  createKing,
+  createSquire,
+  kingLocution,
+} from "../../characters/factories/factories.js";
+import {
+  CharacterWithoutMethodsAndStatus,
+  type Character,
+} from "../../types.js";
+import { getCharacterKindMark } from "../characterCard.js";
+
+describe("Given the getCharacterMark", () => {
+  describe("When it receives a king character", () => {
+    test("Then it should return 👑", () => {
+      const king = createKing(
+        {
+          name: "",
+          age: 5,
+          familyName: "",
+          photoSrc: "",
+        },
+        5,
+      );
+      const expectedMark = "👑";
+
+      const kingMark = getCharacterKindMark(king);
+
+      expect(kingMark).toBe(expectedMark);
+    });
+  });
+
+  describe("When it receives a fighter character", () => {
+    test("Then it should return 🗡 ", () => {
+      const fighter = createFighter(
+        {
+          name: "",
+          age: 5,
+          familyName: "",
+          photoSrc: "",
+        },
+        "Espada",
+        10,
+      );
+      const expectedMark = "🗡";
+
+      const fightertMark = getCharacterKindMark(fighter);
+
+      expect(fightertMark).toBe(expectedMark);
+    });
+  });
+
+  describe("When it receives a counselor character", () => {
+    test("Then it should return 🎓 ", () => {
+      const fighter = createFighter(
+        {
+          name: "",
+          age: 5,
+          familyName: "",
+          photoSrc: "",
+        },
+        "Espada",
+        10,
+      );
+
+      const counselor = createCounselor(
+        { name: "", age: 5, familyName: "", photoSrc: "" },
+        fighter,
+      );
+
+      const expectedMark = "🎓";
+
+      const counselorMark = getCharacterKindMark(counselor);
+
+      expect(counselorMark).toBe(expectedMark);
+    });
+  });
+
+  describe("When it receives a squire character", () => {
+    test("Then it should return 🛡️ ", () => {
+      const fighter = createFighter(
+        {
+          name: "",
+          age: 5,
+          familyName: "",
+          photoSrc: "",
+        },
+        "Espada",
+        10,
+      );
+      const squire = createSquire(
+        { name: "", age: 5, familyName: "", photoSrc: "" },
+        fighter,
+        5,
+      );
+
+      const expectedMark = "🛡️";
+
+      const squireMark = getCharacterKindMark(squire);
+
+      expect(squireMark).toBe(expectedMark);
+    });
+  });
+
+  describe("When it receives a character thats not a king nor a fighter nor counselor nor a squire", () => {
+    test("Then it should return an empty text", () => {
+      const character: Character = {
+        name: "",
+        familyName: "",
+        age: 5,
+        photoSrc: "",
+        isAlive: true,
+        die(this: Character) {
+          this.isAlive = false;
+        },
+        speak() {
+          return kingLocution;
+        },
+      };
+      const expectedMark = "";
+
+      const mark = getCharacterKindMark(character);
+
+      expect(mark).toBe(expectedMark);
+    });
+  });
+});
--- src/ui/characterCard.ts
+++ src/ui/characterCard.ts
@@ -4,12 +4,7 @@ export const createCharacterCard = (character: Character) => {
   const characterElement = document.createElement("li");
   characterElement.classList.add("character");
 
-  const characterPhoto = document.createElement("img");
-  characterPhoto.classList.add("character__photo");
-  characterPhoto.src = character.photoSrc;
-  characterPhoto.width = 300;
-  characterPhoto.height = 255;
-  characterPhoto.alt = `${character.name} ${character.familyName}`;
+  const characterPhoto = addCharacterPhoto(character);
 
   const characterMainDataContainer = document.createElement("div");
   characterMainDataContainer.classList.add("character__main-data");
@@ -23,8 +18,34 @@ export const createCharacterCard = (character: Character) => {
   characterAge.textContent = `Age: ${character.age} years`;
 
   const characterStatus = document.createElement("div");
+  const statusIcon = getStatusIcon(character);
+
   characterStatus.classList.add("character__status");
   characterStatus.textContent = "Status:";
+
+  characterStatus.appendChild(statusIcon);
+
+  characterMainDataContainer.appendChild(characterName);
+  characterMainDataContainer.appendChild(characterAge);
+  characterMainDataContainer.appendChild(characterStatus);
+
+  characterElement.appendChild(characterPhoto);
+  characterElement.appendChild(characterMainDataContainer);
+
+  return characterElement;
+};
+
+const addCharacterPhoto = (character: Character): HTMLImageElement => {
+  const characterPhoto = document.createElement("img");
+  characterPhoto.classList.add("character__photo");
+  characterPhoto.src = character.photoSrc;
+  characterPhoto.width = 300;
+  characterPhoto.height = 255;
+  characterPhoto.alt = `${character.name} ${character.familyName}`;
+  return characterPhoto;
+};
+
+const getStatusIcon = (character: Character): HTMLImageElement => {
   const characterIconSrc = character.isAlive
     ? "images/icons/thumb-up-fill.svg"
     : "images/icons/thumb-down-fill.svg";
@@ -34,12 +55,26 @@ export const createCharacterCard = (character: Character) => {
   statusIcon.width = 25;
   statusIcon.height = 25;
 
-  characterElement.appendChild(characterPhoto);
-  characterStatus.appendChild(statusIcon);
-  characterMainDataContainer.appendChild(characterName);
-  characterMainDataContainer.appendChild(characterAge);
-  characterMainDataContainer.appendChild(characterStatus);
-  characterElement.appendChild(characterMainDataContainer);
+  return statusIcon;
+};
 
-  return characterElement;
+export const getCharacterKindMark = (character: Character) => {
+  let mark = "";
+  if (Object.hasOwn(character, "totalReignYears")) {
+    mark = "👑";
+  }
+
+  if (Object.hasOwn(character, "weapon")) {
+    mark = "🗡";
+  }
+
+  if (Object.hasOwn(character, "counselledCharacter")) {
+    mark = "🎓";
+  }
+
+  if (Object.hasOwn(character, "master")) {
+    mark = "🛡️";
+  }
+
+  return mark;
 };
