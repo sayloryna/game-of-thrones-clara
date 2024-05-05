b30f639d4c498dd509c750d67b5a3509f5d8ef1e
Clara <clarafdezfraga@gmail.com>
2024-05-05 16:05:15 +0200

    Fix killCharacter fetch url

--- src/characters/interactions/interactions.ts
+++ src/characters/interactions/interactions.ts
@@ -4,20 +4,19 @@ import {
   type Locution,
 } from "../types.js";
 
+const renderUrl = `https://game-of-thrones-back-4.onrender.com/characters/`;
+const localHostUrl = `http://localhost:4000/characters/`;
+
 export const speak = async (character: Character): Promise<Locution> => {
-  const response = await fetch(
-    `https://game-of-thrones-back-4.onrender.com/characters/${character.id}`,
-  );
+  const response = await fetch(`${renderUrl}speak/${character.id}`);
+
   const { locution } = (await response.json()) as LocutionResponse;
 
   return locution;
 };
 
 export const killCharacter = async (character: Character): Promise<void> => {
-  const response = await fetch(
-    `https://game-of-thrones-back-4.onrender.com/characters/${character.id}`,
-    { method: "patch" } as RequestInit,
-  );
+  const response = await fetch(`${renderUrl}die/${character.id}`);
 
   if (!response) {
     throw new Error("Character not found");
